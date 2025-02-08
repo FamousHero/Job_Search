60f7498337ed0f4ff381398eae28946ce1e69a9e
Daniel F <danielfranco2011@gmail.com>
2025-02-08 02:57:03 -0800

    Read *.foundation until visit_token, group new entries into single string for later mutations

--- script.py
+++ script.py
@@ -13,14 +13,51 @@
 # add the new entries to the start of the foundation file data  |-------
 # write the data back to the file after truncating              |
 #                           ------------------------------------*
-def main(*args, **kwargs):
-    _foundation_parser(args[0])
 
-def _foundation_parser(filepath: str):
+# Foundation entry example:
+# "Part Time Front Desk Receptionistjob description opens in a new window
+#  Synergy Planning GroupSeal Beach, CA"
+def main(*args, **kwargs):
+    visit_token = 30 * '-'
+    visit_token += '\n'
+
+    _foundation_parser(args[0], visit_token)
+
+def _foundation_parser(filepath: str, visit_token: str):
+    print(visit_token+'Reading Entries...')
     with open(filepath, 'r+') as foundation:
+        lines = []
+        line = foundation.readline()
         # read the lines in the file until "visited" token found
-        data = foundation.read()
-        print(data + "\n" + 30*"-")
+        while line != visit_token:
+            lines.append(line)
+            line = foundation.readline()
+        
+        lines.append('\n')
+        print('End of new entries...\n')
+
+        new_entries = _group_entries(lines) 
+        print('Entries Grouped...') 
+
+        print('\n*.foundation parsed...\nExiting Program\n'+visit_token)
+        print(lines)
+        print(30*'*')
+        print(new_entries)
+
+def _group_entries(lines: list[str]):
+    entries = []
+    entry = ''
+
+    for line in lines:
+        if line == '\n':
+            entries.append(entry)
+            entry = ''    
+            continue
+        if entry.find('job description opens in a new window'):
+            entry = entry.replace('job description opens in a new window', '')
+        entry += line
+
+    return entries
 
 def _update_markdown_file(file: str):
     with open(filepath, 'r+'):
